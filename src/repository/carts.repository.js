9a071dd52a7cdab915cfbca58df5bdc5982239b9
pyoja <wjdgns13nr@naver.com>
2023-02-02 17:02:21 +0900

    feat: 로그인,회원가입,상세페이지 ejs생성

--- src/repository/carts.repository.js
+++ src/repository/carts.repository.js
@@ -1,8 +1,11 @@
 "use strict";
-
+const { Cart } = require("../models");
 
 class CartsRepository {
-  
+  async getCarts(userId) {
+    const carts = await Cart.findAll({ where: { user_id: userId } });
+    return carts;
+  }
 }
 
 module.exports = CartsRepository;
--- src/services/carts.service.js
+++ src/services/carts.service.js
@@ -4,7 +4,10 @@ const CartsRepository = require("../repository/carts.repository.js");
 
 class CartsService {
   cartsRepository = new CartsRepository();
-
+  async getCarts(userId) {
+    const carts = await this.cartsRepository.getCarts(userId);
+    return carts;
+  }
 }
 
 module.exports = CartsService;

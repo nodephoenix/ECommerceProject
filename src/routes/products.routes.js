fe481dff05307cf1d72a3451e54e9cf77bb65203
Park Jin <tstunas@gmail.com>
2023-02-02 16:30:47 +0900

    Fix: 경로의 api 중복 제거

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -7,10 +7,10 @@ const ProductsController = require('../controllers/products.controllers.js');
 const productsController = new ProductsController();
 
 // 전체 상품 목록 조회 API
-router.get('/api/products', );
+router.get('/products', );
 
 // 상품 상세 목록 조회
-router.get('/api/products/:productId',);
+router.get('/products/:productId',);
 
 
 module.exports = router;

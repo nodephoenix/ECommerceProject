ec85fab04a177ee6823991a4b752d670369028d9
OHGILHWAN <dhrlfghks01@gmail.com>
2023-02-03 20:07:32 +0900

    Refactor: orders repository 의존성 주입으로 리팩토링

--- src/routes/orders.routes.js
+++ src/routes/orders.routes.js
@@ -8,19 +8,19 @@ const OrdersController = require("../controllers/orders.controllers.js");
 
 const ordersController = new OrdersController();
 
+// 단일 그림 주문하기(바로 주문) API
+router.post("/orders", authMiddleware, ordersController.orderArt);
+
+// 장바구니 상품 주문하기 API
+router.post("/orders/carts", authMiddleware, ordersController.orderCart);
+
+// 주문 취소하기 API
+router.put("/orders/:orderId", authMiddleware, ordersController.cancelOrder);
+
 // 나의 주문 이력 조회 API
 router.get("/orders", authMiddleware, ordersController.myOrdersList);
 
 // 자기 주문 상세 내역 조회 API
 router.get("/orders/:orderId", authMiddleware, ordersController.orderDetail);
-
-// 장바구니 상품 주문하기 API
-router.post("/orders/carts", authMiddleware, ordersController.orderCart);
-
-// 단일 그림 주문하기(바로 주문) API
-router.post("/orders", authMiddleware, ordersController.orderArt);
-
-// 주문 취소하기 API
-router.put("/orders/:orderId", authMiddleware, ordersController.cancelOrder);
 
 module.exports = router;

f24d5258beafbe0bb667311d19dab6213c8bf729
Park Jin <tstunas@gmail.com>
2023-02-03 13:30:35 +0900

    feat: admin 컨트롤러의 메소드에 wrapAsync 적용

--- src/controllers/admin.controllers.js
+++ src/controllers/admin.controllers.js
@@ -17,14 +17,14 @@ class AdminController {
    * @param {Response} res
    * @param {NextFunction} next
    */
-  registerProducts = async (req, res, next) => {
+  registerProducts = wrapAsync(async (req, res, next) => {
     /** @type {{productName: string; desc: string; price: number; image: string;}} */
     const body = req.body;
     await this.adminService.registerProducts(body);
     res.json({
       message: "상품 등록이 완료되었습니다.",
     });
-  };
+  });
 
   /**
    *
@@ -32,7 +32,7 @@ class AdminController {
    * @param {Response} res
    * @param {NextFunction} next
    */
-  editProducts = async (req, res, next) => {
+  editProducts = wrapAsync(async (req, res, next) => {
     const { productId } = req.params;
     /** @type {{productName: string; desc: string; price: number; image: string;}} */
     const body = req.body;
@@ -40,7 +40,7 @@ class AdminController {
     res.json({
       message: "상품 정보가 변경되었습니다",
     });
-  };
+  });
 
   /**
    *
@@ -48,13 +48,13 @@ class AdminController {
    * @param {Response} res
    * @param {NextFunction} next
    */
-  deleteProducts = async (req, res, next) => {
+  deleteProducts = wrapAsync(async (req, res, next) => {
     const { productId } = req.params;
     await this.adminService.deleteProducts(Number(productId));
     res.json({
       message: "상품을 삭제하였습니다.",
     });
-  };
+  });
 
   /**
    *
@@ -62,10 +62,10 @@ class AdminController {
    * @param {Response} res
    * @param {NextFunction} next
    */
-  getOrderProducts = async (req, res, next) => {
+  getOrderProducts = wrapAsync(async (req, res, next) => {
     const orders = await this.adminService.getOrderProducts();
     res.json(orders);
-  };
+  });
 
   /**
    *
@@ -73,7 +73,7 @@ class AdminController {
    * @param {Response} res
    * @param {NextFunction} next
    */
-  putProductsStatus = async (req, res, next) => {
+  putProductsStatus = wrapAsync(async (req, res, next) => {
     const { productId } = req.params;
     /** @type {{status: string}} */
     const { status } = req.body;
@@ -81,7 +81,7 @@ class AdminController {
     res.json({
       message: "상품 상태를 변경하였습니다.",
     });
-  };
+  });
 
   /**
    *
@@ -89,7 +89,7 @@ class AdminController {
    * @param {Response} res
    * @param {NextFunction} next
    */
-  putUserGrade = async (req, res, next) => {
+  putUserGrade = wrapAsync(async (req, res, next) => {
     const { userId } = req.params;
     /** @type {{grade: number}} */
     const { grade } = req.body;
@@ -97,7 +97,7 @@ class AdminController {
     res.json({
       message: "등급이 변경되었습니다.",
     });
-  };
+  });
 }
 
 module.exports = AdminController;

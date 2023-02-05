3abb355d9d7f7fa0744f8dbeb74fdacebf5045a0
Park Jin <tstunas@gmail.com>
2023-02-05 22:28:18 +0900

    Test: admin integration 테스트

--- __test__/integration/admin.integration.spec.js
+++ __test__/integration/admin.integration.spec.js
@@ -1,22 +1,20 @@
 "use strict";
 // @ts-check
 
+// require('dotenv').config('../../.env')
+const jwt = require("jsonwebtoken");
 const supertest = require("supertest");
 const app = require("../../src/app");
 
-const userSeed = require("../../sequelize/seeders/20230203014824-user");
-const productSeed = require("../../sequelize/seeders/20230203024610-product");
-const orderSeed = require("../../sequelize/seeders/20230203043628-order");
-const orderProductSeed = require("../../sequelize/seeders/20230203043637-order_product");
-const cartSeed = require("../../sequelize/seeders/20230203043656-cart");
-
 const db = require("../../sequelize/models/index.js");
+const seederHelper = require("../helper/seeder.helper");
 /** @type {import('sequelize').Sequelize} */
 const sequelize = db.sequelize;
 
+let token;
 beforeAll(async () => {
   if (process.env.NODE_ENV === "test") {
-    await sequelize.sync();
+    await sequelize.sync({ force: true });
   } else {
     throw new Error("NODE_ENV가 test 환경으로 설정되어 있지 않습니다.");
   }
@@ -24,33 +22,80 @@ beforeAll(async () => {
 
 beforeEach(async () => {
   const queryInterface = sequelize.getQueryInterface();
-    await userSeed.up(queryInterface)
-    await productSeed.up(queryInterface)
-    await orderSeed.up(queryInterface)
-    await orderProductSeed.up(queryInterface)
-    await cartSeed.up(queryInterface)
-});
-
-describe("Admin Domain Integration Test", () => {
-  test("GET api/admin/products (GetProducts)", async () => {
-    const response = await supertest(app)
-      .get("/api/admin/products")
-      .query({})
-      .send({});
-
-    expect(response.status).toEqual(200);
-    expect(response.body).toEqual({ data: [] });
+  await seederHelper.up(queryInterface);
+  // 어드민 유저로 로그인
+  token = jwt.sign({ id: 1 }, process.env.JWT_SECRET_KEY, {
+    algorithm: "HS256", // 해싱 알고리즘
+    expiresIn: process.env.JWT_EXPIRES_IN, // 토큰 유효 기간
+    issuer: "issuer", // 발행자
   });
 });
 
+describe("Admin Domain Integration Test", () => {
+  test("POST api/admin/products (RegisterProducts)", async () => {
+    const response = await supertest(app)
+    .post("/api/admin/products")
+    .set("Cookie", `x_auth=${token}`)
+    .send({})
+
+    expect(response.status).toEqual(201);
+  });
+
+  test("PUT api/admin/products/:productId (editProducts)", async () => {
+    const response = await supertest(app)
+    .put("/api/admin/products/1")
+    .set("Cookie", `x_auth=${token}`)
+    .send({})
+
+    expect(response.status).toEqual(201);
+  });
+
+  test("DELETE api/admin/products/:productId (deleteProducts)", async () => {
+    const response = await supertest(app)
+    .put("/api/admin/products/1")
+    .set("Cookie", `x_auth=${token}`)
+    .send({})
+
+    expect(response.status).toEqual(201);
+  });
+
+
+  test("GET api/admin/products (GetProducts)", async () => {
+    const response = await supertest(app)
+      .get("/api/admin/products")
+      .set("Cookie", `x_auth=${token}`)
+      .send({});
+
+    expect(response.status).toEqual(200);
+  });
+
+  
+  test("PUT api/admin/products/:productId/status (putProductsStatus)", async () => {
+    const response = await supertest(app)
+    .put("/api/admin/products/1/status")
+    .set("Cookie", `x_auth=${token}`)
+    .send({
+      status: 1
+    })
+
+    expect(response.status).toEqual(201);
+  });
+  
+  test("PUT api/admin/user/:userId/grade (putProductsStatus)", async () => {
+    const response = await supertest(app)
+    .put("/api/admin/user/2/grade")
+    .set("Cookie", `x_auth=${token}`)
+    .send({
+      grade: 1
+    })
+
+    expect(response.status).toEqual(201);
+  });
+});
 
 afterEach(async () => {
   const queryInterface = sequelize.getQueryInterface();
-    await userSeed.down(queryInterface)
-    await productSeed.down(queryInterface)
-    await orderSeed.down(queryInterface)
-    await orderProductSeed.down(queryInterface)
-    await cartSeed.down(queryInterface)
+  await seederHelper.down(queryInterface);
 });
 
 afterAll(async () => {

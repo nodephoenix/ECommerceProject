39e35d186793cbad0bac9ee75e3a10edd6965778
OHGILHWAN <dhrlfghks01@gmail.com>
2023-02-07 04:47:55 +0900

    Fix: 장바구니 상품 수량 변경 bug fix

--- src/routes/orders.routes.js
+++ src/routes/orders.routes.js
@@ -11,7 +11,7 @@ const ordersController = new OrdersController();
 // 단일 그림 주문하기(바로 주문) API o
 router.post("/orders", authMiddleware, ordersController.orderArt);
 
-// 장바구니 상품 주문하기 API x
+// 장바구니 상품 주문하기 API o
 router.post("/orders/carts", authMiddleware, ordersController.orderCart);
 
 // 주문 취소하기 API x

afcb8eb92d1510e2c5f5090c070aec1ca37e4b25
Park Jin <tstunas@gmail.com>
2023-02-07 12:21:35 +0900

    Test: auth controller test 사전 require

--- __test__/unit/controllers/auth.conrollers.spec.js
+++ __test__/unit/controllers/auth.conrollers.spec.js
@@ -1,13 +1,28 @@
 "use strict";
 // @ts-check
 
+/** @typedef {import('../../../src/services/auth.service')} AuthServiceType */
+// AuthController 내부에서 생성되어 들어가는 서비스 클래스
+// 서비스 클래스를 모킹화하여 실제 서비스가 안 들어가도록 한다.
+/** @type {jest.MockInstance<AuthServiceType>} */
+const AuthService = require("../../../src/services/auth.service");
 const AuthController = require("../../../src/controllers/auth.controllers");
+jest.mock("../../../src/services/auth.service");
 
+const mockRequest = require("../../mock/request.mock");
+const mockResponse = require("../../mock/response.mock");
+
+/** @type {AuthController} */
+let authController;
+/** @type {jest.Mocked<AuthServiceType>} */
+let authService;
 describe("auth controller 유닛 테스트", () => {
   /** @type {AuthController} */
   let authController;
   beforeEach(() => {
+    AuthService.mockClear();
     authController = new AuthController();
+    authService = authController.authService;
   });
 
   test("AuthController 인스턴스 생성 테스트", () => {

33f2de46b037f1d9b910f17ad93d6ef28ddc26cc
5kiran <118159013+5kiran@users.noreply.github.com>
2023-02-07 19:53:08 +0900

    Revert "Front/ajax"

--- sequelize/config/config.js
+++ sequelize/config/config.js
@@ -1,8 +1,8 @@
-require("dotenv").config("../../.env");
+require("dotenv").config('../../.env');
 
 const development = {
   username: process.env.SEQUELIZE_DEVELOPMENT_USERNAME || "root",
-  password: process.env.SEQUELIZE_DEVELOPMENT_PASSWORD || "ghkswns139",
+  password: process.env.SEQUELIZE_DEVELOPMENT_PASSWORD || null,
   database: process.env.SEQUELIZE_DEVELOPMENT_DATABASE || "nodephoenix",
   host: process.env.SEQUELIZE_DEVELOPMENT_HOST || "127.0.0.1",
   dialect: "mysql",
@@ -10,7 +10,7 @@ const development = {
 
 const test = {
   username: process.env.SEQUELIZE_TEST_USERNAME || "root",
-  password: process.env.SEQUELIZE_TEST_PASSWORD || "ghkswns139",
+  password: process.env.SEQUELIZE_TEST_PASSWORD || null,
   database: process.env.SEQUELIZE_TEST_DATABASE || "nodephoenix_test",
   host: process.env.SEQUELIZE_TEST_HOST || "127.0.0.1",
   dialect: "mysql",
@@ -18,7 +18,7 @@ const test = {
 
 const production = {
   username: process.env.SEQUELIZE_PRODUCTION_USERNAME || "root",
-  password: process.env.SEQUELIZE_PRODUCTION_PASSWORD || "ghkswns139",
+  password: process.env.SEQUELIZE_PRODUCTION_PASSWORD || null,
   database: process.env.SEQUELIZE_PRODUCTION_DATABASE || "nodephoenix_prod",
   host: process.env.SEQUELIZE_PRODUCTION_HOST || "127.0.0.1",
   dialect: "mysql",

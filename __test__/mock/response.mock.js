ee65f6cc87dde4e6ad491ec543a3303f616a27ba
Park Jin <tstunas@gmail.com>
2023-02-07 13:52:43 +0900

    Test: mockResponse 생성함수에 send 메소드 추가

--- __test__/mock/response.mock.js
+++ __test__/mock/response.mock.js
@@ -8,5 +8,6 @@ module.exports = (data = {}) => {
   res.status = jest.fn().mockReturnValue(res);
   res.json = jest.fn().mockReturnValue(res);
   res.cookie = jest.fn().mockReturnValue(res);
+  res.send = jest.fn().mockRejectedValue(res);
   return res;
 };

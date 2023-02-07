2bc148a2a6c34cff46554b202084bc0c79b44125
Park Jin <tstunas@gmail.com>
2023-02-08 00:53:26 +0900

    Test: UsersService 유닛 테스트

--- __test__/unit/services/users.service.spec.js
+++ __test__/unit/services/users.service.spec.js
@@ -1,16 +1,59 @@
 "use strict";
 // @ts-check
 
+/** @typedef {import('../../../src/repository/users.repository')} UsersRepositoryType */
+// UsersController 내부에서 생성되어 들어가는 서비스 클래스
+// 서비스 클래스를 모킹화하여 실제 서비스가 안 들어가도록 한다.
+/** @type {jest.MockInstance<UsersRepositoryType>} */
+const UsersRepository = require("../../../src/repository/users.repository");
 const UsersService = require("../../../src/services/users.service");
+jest.mock("../../../src/repository/users.repository");
 
-describe("users service 유닛 테스트", () => {
-  /** @type {UsersService} */
-  let usersService;
-  beforeEach(() => {
-    usersService = new UsersService();
+/** @type {UsersService} */
+let usersService;
+/** @type {jest.Mocked<UsersRepositoryType>} */
+let usersRepository;
+beforeEach(() => {
+  UsersRepository.mockClear();
+  usersService = new UsersService();
+  usersRepository = usersService.usersRepository;
+  usersRepository.findUserInfo = jest.fn();
+  usersRepository.updateUserInfo = jest.fn();
+});
+
+describe("UsersService", () => {
+  test("create Instance of UsersService", () => {
+    expect(usersService).toBeTruthy();
+  });
+});
+
+describe("UsersService.userInfo", () => {
+  test("method exist", () => {
+    expect(usersService.userInfo).toBeTruthy();
   });
 
-  test("UsersService 인스턴스 생성 테스트", () => {
-    expect(usersService).toBeTruthy();
+  test("method success", () => {
+    const userId = 3;
+    const userInfo = {};
+    usersRepository.findUserInfo.mockResolvedValue(userInfo);
+    expect(usersService.userInfo(userId)).resolves.toStrictEqual(userInfo);
+  });
+});
+
+describe("UsersService.updateUserInfo", () => {
+  test("method exist", () => {
+    expect(usersService.updateUserInfo).toBeTruthy();
+  });
+
+  test("method success", () => {
+    const userId = 3;
+    const userName = "testman";
+    const email = "test@gmail.com";
+    const phone = "010-1234-5678";
+    const updateUserInfo = {};
+    usersRepository.updateUserInfo.mockResolvedValue(updateUserInfo);
+    expect(
+      usersService.updateUserInfo(userId, userName, email, phone)
+    ).resolves.toStrictEqual(updateUserInfo);
   });
 });
